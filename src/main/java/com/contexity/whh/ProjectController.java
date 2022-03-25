9c9a57f50f6c4e649bfdb7a41a2a4e8ba192a50d
bmit-ded <53931396+bmit-ded@users.noreply.github.com>
2022-03-25 10:24:14 +0100

    Deleted code which isnt needed

--- src/main/java/com/contexity/whh/ProjectController.java
+++ src/main/java/com/contexity/whh/ProjectController.java
@@ -1,5 +1,10 @@
 package com.contexity.whh;
 
+import com.contexity.whh.repository.ProjectRepository;
+import com.contexity.whh.service.ProjectService;
+import com.contexity.whh.service.dto.ProjectDTO;
+import java.util.Optional;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,9 +13,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProjectController {
 
-    @RequestMapping("/stats/projects/{projectid}")
-    public String getData(@PathVariable Integer projectid, Model model) {
-        model.addAttribute("projectid", projectid);
+    private final ProjectService projectService;
+
+    private final ProjectRepository projectRepository;
+
+    public ProjectController(ProjectService projectService, ProjectRepository projectRepository) {
+        this.projectService = projectService;
+        this.projectRepository = projectRepository;
+    }
+
+    @RequestMapping("/stats/projects/{id}")
+    public String get(@PathVariable Integer id, Model model) {
+        Optional<ProjectDTO> projectDTO = projectService.findOne(id.longValue());
+        model.addAttribute("project", projectDTO.get());
 
         return "projects";
     }
